c87fc6cad04be751cede7cbf46a35f910d28067f
Daffa Ilham Ramadan <daffailhamramadan127@gmail.com>
2022-01-21 23:53:04 +0700

    Add Random Dungeon Generator

---
+++ Assets/Scripts/Random Dungeon Generator/RoomSpawn.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawn : MonoBehaviour
+{
+    private RoomTemplates roomTemplates;
+
+    private int randomIndex;
+
+    public enum DoorDirection
+    {
+        Bottom,
+
+        Top,
+
+        Left,
+
+        Right,
+
+        Zero
+    }
+
+    public DoorDirection doorDirection;
+
+    public bool spawned;
+  
+    private void Awake()
+    {
+        roomTemplates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+    }
+
+    private void Start()
+    {
+        Invoke("Spawn", 1f);
+    }
+
+    private void Spawn()
+    {
+        if(spawned == false)
+        {
+            if (doorDirection == DoorDirection.Bottom)
+            {
+                randomIndex = Random.Range(0, roomTemplates.roomBottom.Length);
+
+                Instantiate(roomTemplates.roomBottom[randomIndex], transform.position, Quaternion.identity);
+            }
+            else if (doorDirection == DoorDirection.Top)
+            {
+                randomIndex = Random.Range(0, roomTemplates.roomTop.Length);
+
+                Instantiate(roomTemplates.roomTop[randomIndex], transform.position, Quaternion.identity);
+            }
+            else if (doorDirection == DoorDirection.Left)
+            {
+                randomIndex = Random.Range(0, roomTemplates.roomLeft.Length);
+
+                Instantiate(roomTemplates.roomLeft[randomIndex], transform.position, Quaternion.identity);
+            }
+
+            else if (doorDirection == DoorDirection.Right)
+            {
+                randomIndex = Random.Range(0, roomTemplates.roomRight.Length);
+
+                Instantiate(roomTemplates.roomRight[randomIndex], transform.position, Quaternion.identity);
+            }
+
+            spawned = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SpawnPoints"))
+        {
+            if(collision.GetComponent<RoomSpawn>().spawned == false && spawned == false)
+            {
+                Instantiate(roomTemplates.closedRoom, transform.position, Quaternion.identity);
+            }
+
+            spawned = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Random Dungeon Generator/RoomTemplates.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] roomBottom;
+
+    public GameObject[] roomTop;
+
+    public GameObject[] roomLeft;
+
+    public GameObject[] roomRight;
+
+    public GameObject closedRoom;
+}
